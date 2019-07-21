714d3157ffa2a71456f82e24efd919821c078174
Douglas Muth <dmuth@dmuth.org>
2019-07-21 17:37:30 -0400

    First draft--code written to parse sleep logs.

---
+++ bin/parse-fitbit-logs.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+#
+# Vim: :set tabstop=4
+#
+# Go through our Fitbit logs in a directory specified on the command line
+# and write them out in /logs in a format that Splunk will understand.
+#
+
+
+import argparse
+import datetime
+import json
+import logging
+import os
+import re
+import sys
+
+log_dir = "/logs/"
+
+logging.basicConfig(level = logging.INFO, format='%(asctime)s.%(msecs)03d: %(levelname)s: %(message)s',
+	datefmt = '%Y-%m-%d %H:%M:%S'
+	)
+
+
+parser = argparse.ArgumentParser(description = 
+	"Parse Fitbit logs and write them out in files for Splunk")
+parser.add_argument('directory', metavar = 'DIRECTORY', type = str, nargs = 1,
+                    help = "Path to user-site-export/ direcotry in your Fitbit export")
+
+args = parser.parse_args()
+logging.info("Args: {}".format(args))
+
+
+#
+# Extract key pieces of data from a row of Fitbit's sleep data.
+#
+def sleepRowToData(row):
+
+	retval = {}
+
+	retval["dateOfSleep"] = row["dateOfSleep"] + "T00:00:00.000"
+	retval["minutesAsleep"] = row["minutesAsleep"]
+	retval["minutesAwake"] = row["minutesAwake"]
+	retval["startTime"] = row["startTime"]
+	retval["endTime"] = row["endTime"]
+	retval["timeInBed"] = row["timeInBed"]
+	retval["efficiency"] = row["efficiency"]
+
+	return(retval)
+
+
+#
+# Loop through our directory for sleep logs
+#
+def loopSleepLogs(dir):
+
+	output_file = log_dir + "sleep.json"
+	logging.info("Opening output file {}...".format(output_file))
+	output = open(output_file, "w")
+
+	for filename in os.listdir(dir):
+
+		if not filename.startswith("sleep-"):
+			continue
+
+		filename = dir + "/" + filename
+		logging.info("Reading file {}...".format(filename))
+
+		file = open(filename)
+		rows = json.load(file)
+		file.close()
+
+		logging.info("Writing {} rows to {}...".format(len(rows), output_file))
+		for row in rows:
+			data = sleepRowToData(row)
+			output.write(json.dumps(data) + "\n")
+
+	output.close()
+
+
+
+#
+# Our main entry point.
+#
+def main(args):
+
+	directory = args.directory[0]
+
+	if not os.path.exists(directory):
+		raise Exception("Path {} does not exist!".format(directory))
+
+	loopSleepLogs(directory)
+	
+	# TEST/TODO:
+	# - Loop through heartrate files
+	# - Open each file, print output to /logs/heartrate.json
+	#
+
+main(args)
+
+
