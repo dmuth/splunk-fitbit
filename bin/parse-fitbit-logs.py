d7fa1469e49e7d6e4eaa37224e9f07510f2bb1ca
Douglas Muth <dmuth@dmuth.org>
2019-07-21 18:35:24 -0400

    Hardcoding directory to /mnt/fitbit/, which should make running in both dev/prod a lot more straightforward

--- bin/parse-fitbit-logs.py
+++ bin/parse-fitbit-logs.py
@@ -23,8 +23,6 @@ logging.basicConfig(level = logging.INFO, format='%(asctime)s.%(msecs)03d: %(lev
 
 parser = argparse.ArgumentParser(description = 
 	"Parse Fitbit logs and write them out in files for Splunk")
-parser.add_argument('--directory', metavar = 'DIRECTORY', type = str, required = True,
-                    help = "Path to user-site-export/ direcotry in your Fitbit export")
 parser.add_argument('--num-days-sleep', type = int, default = 90,
                     help = "How many days of sleep data to process (default: 90)")
 parser.add_argument('--num-days-heartrate', type = int, default = 30,
@@ -153,11 +151,12 @@ def loopHeartrateLogs(dir, num_days):
 #
 def main(args):
 
-	if not os.path.exists(args.directory):
-		raise Exception("Path {} does not exist!".format(args.directory))
+	directory = "/mnt/fitbit/"
+	if not os.path.exists(directory):
+		raise Exception("Path {} does not exist!".format(directory))
 
-	loopSleepLogs(args.directory, args.num_days_sleep)
-	loopHeartrateLogs(args.directory, args.num_days_heartrate)
+	loopSleepLogs(directory, args.num_days_sleep)
+	loopHeartrateLogs(directory, args.num_days_heartrate)
 	
 main(args)
 
