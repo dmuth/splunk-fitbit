f685921fc05f7433f48556ef39f53fae1b767a10
Douglas Muth <dmuth@dmuth.org>
2019-07-21 18:37:16 -0400

    Added in --devel code and changed script call to exec

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,15 @@
 # Entrypoint script--determine what it is we want to do.
 #
 
+if test "$1" == "--devel"
+then
+	echo "# "
+	echo "# Starting container in devel mode and spawning a bash shell..."
+	echo "# "
+	exec "/bin/bash"
 
-/app/parse-fitbit-logs.py $@
+else
+	exec /app/parse-fitbit-logs.py $@
+
+fi
 
